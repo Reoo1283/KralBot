7e81dea0ff72da4f3dddf4b638f3b8c6d2194af7
Reoo1283 <suleymanderste@hotmail.com>
2026-08-24 14:56:23 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Haxball Baba Futsal!", // required by Discord API, not shown in the client
         state: "BaBa Haxball için geliştirmeler için çalışyor!",     // this is what people actually see
         type: 0,               // Custom
       },
